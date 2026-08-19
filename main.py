bc6b94e80723e9e39fd1b2f3b0c2c5de16f8664a
aniket24122000 <your_email@gmail.com>
2026-08-19 20:41:50 +0530

    add file5

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-import os
+import requests
 from fastapi import FastAPI
 
 app = FastAPI()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL environment variable is missing")
-
 @app.get("/")
 def home():
+    response = requests.get("https://httpbin.org/get")
+
     return {
-        "message": "Application is running",
-        "database": DATABASE_URL
+        "message": "API is working",
+        "status": response.status_code
     }
