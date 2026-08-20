6d921e0c519d5fb2f59a1d9a78bcc45f5dcba681
aniket24122000 <your_email@gmail.com>
2026-08-20 21:19:58 +0530

    add k4 OOMkill

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-import os
 from fastapi import FastAPI
 
 app = FastAPI()
 
+data = []
+
 @app.get("/")
 def home():
-    return {"message": "Hello Kubernetes"}
+    return {"message": "FastAPI running"}
+
+@app.get("/memory")
+def consume_memory():
+    # Intentionally allocate large memory
+    data.append("A" * (100 * 1024 * 1024))
+
+    return {
+        "message": "Memory allocated",
+        "chunks": len(data)
+    }
