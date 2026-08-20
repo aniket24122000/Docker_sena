441468efd6a94bf668dfdefc4b2e5a90ba1ba0c6
aniket24122000 <your_email@gmail.com>
2026-08-20 20:07:34 +0530

    add k1

--- main.py
+++ main.py
@@ -1,29 +1,8 @@
+import os
 from fastapi import FastAPI
-from pathlib import Path
 
 app = FastAPI()
 
-DATA_DIR = Path("/app/data")
-DATA_DIR.mkdir(exist_ok=True)
-
-@app.post("/save")
-def save_data():
-    file = DATA_DIR / "users.txt"
-
-    with open(file, "a") as f:
-        f.write("Aniket\n")
-
-    return {"message": "Data saved"}
-
-
-@app.get("/users")
-def get_users():
-    file = DATA_DIR / "users.txt"
-
-    if not file.exists():
-        return {"users": []}
-
-    with open(file, "r") as f:
-        users = f.read().splitlines()
-
-    return {"users": users}
+@app.get("/")
+def home():
+    return {"message": "Hello Kubernetes"}
