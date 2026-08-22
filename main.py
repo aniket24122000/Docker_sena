0abc43fa9b6c72c39f87366808f8b08ba948a583
aniket24122000 <your_email@gmail.com>
2026-08-22 13:33:10 +0530

    add k9 wrong config map insted of production i gave development

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import os
+
 from fastapi import FastAPI
 
-DATABASE_URL = os.environ["DATABASE_URL"]
-
 app = FastAPI()
+
+APP_ENV = os.getenv("APP_ENV", "unknown")
 
 @app.get("/")
 def home():
     return {
-        "message": "FastAPI is running"
+        "message": "FastAPI is running",
+        "environment": APP_ENV
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status": "UP",
+        "environment": APP_ENV
     }
